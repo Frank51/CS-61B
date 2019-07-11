b388115b32026660e47170e4262fe27c014194ac
Yunqian Chen <yunqianc@andrew.cmu.edu>
2019-07-10 23:15:57 -0400

    add helloNumbers.java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,17 @@ public class LeapYear {
         }
     }
 
+    /** Check whether this year us leap year. 
+     * @param  year the year to be analyzed.
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
